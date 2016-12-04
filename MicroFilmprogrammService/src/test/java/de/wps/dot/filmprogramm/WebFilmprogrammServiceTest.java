d0f17da1472ed0d2bdb8825c964d5861bfeeca66
Florian von Stosch <florian.vonstosch@wps.de>
2016-12-04 21:55:31 +0100

    Initialer Stand

---
+++ MicroFilmprogrammService/src/test/java/de/wps/dot/filmprogramm/WebFilmprogrammServiceTest.java
@@ -0,0 +1,14 @@
+package de.wps.dot.filmprogramm;
+
+import org.junit.Test;
+
+import com.mashape.unirest.http.exceptions.UnirestException;
+
+public class WebFilmprogrammServiceTest {
+
+	@Test
+	public void callHTTPEndPoint() throws UnirestException {
+		// TODO
+	}
+
+}
