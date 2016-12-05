629c9f456fcfdf0b83c03a7f0f253b4abc0880be
Johannes Bumüller <j.bumueller@gmail.com>
2016-12-05 14:31:49 +0100

    Service-Lookup via REST

--- MicroFilmprogrammServiceClient/src/main/java/de/wps/dot/filmprogramm/FilmprogrammClientProxy.java
+++ MicroFilmprogrammServiceClient/src/main/java/de/wps/dot/filmprogramm/FilmprogrammClientProxy.java
@@ -3,9 +3,13 @@ package de.wps.dot.filmprogramm;
 import java.lang.reflect.Type;
 import java.net.URI;
 import java.util.List;
+import java.util.Random;
 
 import javax.naming.NamingException;
 
+import com.ecwid.consul.v1.ConsulClient;
+import com.ecwid.consul.v1.Response;
+import com.ecwid.consul.v1.catalog.model.CatalogService;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 import com.mashape.unirest.http.HttpResponse;
@@ -17,8 +21,10 @@ public class FilmprogrammClientProxy {
 
 	public List<Vorführung> getVorführungen() throws UnirestException, NamingException {
 		List<Vorführung> freeSeats = null;
-
-		URI uri = URI.create("http://localhost:50000/vorführungen");
+				
+		String serviceIp = getServiceAddressByREST();
+		
+		URI uri = URI.create("http://"+serviceIp+":50000/vorführungen");
 
 		HttpResponse<String> response = Unirest.get(uri.toString()).asString();
 		Type seatListType = new TypeToken<List<Vorführung>>() {
@@ -26,5 +32,19 @@ public class FilmprogrammClientProxy {
 		freeSeats = gson.fromJson(response.getBody(), seatListType);
 
 		return freeSeats;
+	}
+
+	private String getServiceAddressByREST() {
+		ConsulClient consulClient = new ConsulClient();
+		List<CatalogService> catalogServices = consulClient.getCatalogService("filmprogramm", null).getValue();
+		
+		if(catalogServices.isEmpty())
+			; // TODO nächster Workshop ;-)
+		
+		int index = new Random().nextInt(catalogServices.size());
+		CatalogService service = catalogServices.get(index);
+
+		String serviceIp = service.getAddress();
+		return serviceIp;
 	}
 }
